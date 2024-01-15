88fe7431fb8895e754aeebbac8f3ba52a69cc435
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-01-15 20:42:10 +0300

    Fix

---
+++ src/main.js
@@ -0,0 +1,104 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
+
+
+const lightbox = new SimpleLightbox('.js-list a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    close: true,
+    enableKeyboard: true,
+    docClose: true,
+});
+
+
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", onSearch);
+const list = document.querySelector(".js-list");
+const loader = document.querySelector(".loader");
+const button = document.querySelector(".js-button");
+button.addEventListener("click", loadMore);
+
+const domRect = list.getBoundingClientRect().width;
+console.log(domRect);
+window.scrollTo(0, `${domRect}`);
+
+
+let currentPage = 1;
+let wordForSearch;
+button.style.display = 'none';
+
+function loadMore() {
+    currentPage += 1;
+    loader.style.display = 'block';
+    getPhoto(wordForSearch,currentPage)
+};
+
+
+function onSearch(evt) {
+    evt.preventDefault();
+    wordForSearch = evt.currentTarget.elements.input.value;
+    getPhoto(wordForSearch);
+    button.style.display = 'block';
+};
+
+
+async function getPhoto(wordForSearch,page=1) {
+    loader.style.display = 'block';
+    await axios.get( "https://pixabay.com/api/",{
+        params: {
+        key: "41617344-22077a3acba128accdfbcd745",
+		q:`${wordForSearch}`,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+        page: `${page}`,
+        per_page: "40"
+	}
+    }).then(responce => {  
+        if (!responce.data.total) {
+            button.style.display = 'none'
+            form.reset()
+            iziToast.error({
+                position: 'topRight',
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            })
+        }
+        
+        loader.style.display = 'none';
+        list.insertAdjacentHTML("beforeend",createMarkup(responce.data.hits))
+        lightbox.refresh();
+        
+    })
+    .catch(error =>  iziToast.error({
+                position:'topRight',
+                message: `"${error}"`
+            }))
+};
+
+
+function createMarkup(arr) {
+    return arr.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) =>` <li>
+    <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" width="350px" height="250px"></a>
+    <ul class="discription-list">
+    <li class="discription-item">
+        <h2>Likes</h2>
+        <p>${likes}</p></li>
+    <li class="discription-item">
+        <h2>Views</h2>
+        <p>${views}</p>
+    </li>
+    <li class="discription-item">
+        <h2>Comments</h2>
+        <p>${comments}</p>
+    </li>
+    <li class="discription-item">
+        <h2>Download</h2>
+        <p>${downloads}</p>
+    </li>
+    </ul>
+    </li>`
+    ).join('');
+};
